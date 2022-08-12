7321cf25dd34938357dab76788257043a13cae79
Sayyid Omid Mousavi Mehr <s.omid.m.mehr@gamil.com>
2022-08-12 23:37:51 +0430

    server_1: migrations + account serializers

---
+++ server_1/account/serializers.py
@@ -0,0 +1,10 @@
+from django_grpc_framework import proto_serializers
+from .models import CustomUser
+from protos import account_pb2
+
+
+class PostProtoSerializer(proto_serializers.ModelProtoSerializer):
+    class Meta:
+        model = CustomUser
+        proto_class = account_pb2.User
+        fields = ['id', 'username', 'email', 'groups']
