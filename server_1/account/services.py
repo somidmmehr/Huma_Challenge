f6459660092eeac7accb9798b43f81e9a471257d
Sayyid Omid Mousavi Mehr <s.omid.m.mehr@gamil.com>
2022-08-13 22:47:07 +0430

    Implemented usage of generics for service django_grpc_framework

--- server_1/account/services.py
+++ server_1/account/services.py
@@ -1,42 +1,48 @@
 import grpc
+from django_grpc_framework import generics
 from google.protobuf import empty_pb2
 from django_grpc_framework.services import Service
 from .models import CustomUser
 from .serializers import UserProtoSerializer
 
 
-class CustomUserService(Service):
-    def List(self, request, context):
-        user = CustomUser.objects.all()
-        serializer = UserProtoSerializer(user, many=True)
-        for msg in serializer.message:
-            yield msg
+class CustomUserService(generics.ModelService):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserProtoSerializer
 
-    def Create(self, request, context):
-        serializer = UserProtoSerializer(message=request)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return serializer.message
 
-    def get_object(self, pk):
-        try:
-            return CustomUser.objects.get(pk=pk)
-        except CustomUser.DoesNotExist:
-            self.context.abort(grpc.StatusCode.NOT_FOUND, f'User id {pk} not found!')
-
-    def Retrieve(self, request, context):
-        user = self.get_object(request.id)
-        serializer = UserProtoSerializer(user)
-        return serializer.message
-
-    def Update(self, request, context):
-        user = self.get_object(request.id)
-        serializer = UserProtoSerializer(user, message=request)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return serializer.message
-
-    def Destroy(self, request, context):
-        user = self.get_object(request.id)
-        user.delete()
-        return empty_pb2.Empty()
+# class CustomUserService(Service):
+#     def List(self, request, context):
+#         user = CustomUser.objects.all()
+#         serializer = UserProtoSerializer(user, many=True)
+#         for msg in serializer.message:
+#             yield msg
+#
+#     def Create(self, request, context):
+#         serializer = UserProtoSerializer(message=request)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return serializer.message
+#
+#     def get_object(self, pk):
+#         try:
+#             return CustomUser.objects.get(pk=pk)
+#         except CustomUser.DoesNotExist:
+#             self.context.abort(grpc.StatusCode.NOT_FOUND, f'User id {pk} not found!')
+#
+#     def Retrieve(self, request, context):
+#         user = self.get_object(request.id)
+#         serializer = UserProtoSerializer(user)
+#         return serializer.message
+#
+#     def Update(self, request, context):
+#         user = self.get_object(request.id)
+#         serializer = UserProtoSerializer(user, message=request)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return serializer.message
+#
+#     def Destroy(self, request, context):
+#         user = self.get_object(request.id)
+#         user.delete()
+#         return empty_pb2.Empty()
